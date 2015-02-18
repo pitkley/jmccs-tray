484f8d1257af5ab51ee554f058f0377c8c6e1811
Pit Kleyersburg <pitkley@googlemail.com>
2015-02-18 21:38:08 +0100

    Basic tray

--- src/main/java/de/pitkley/jmccs/tray/Main.java
+++ src/main/java/de/pitkley/jmccs/tray/Main.java
@@ -1,8 +1,41 @@
 package de.pitkley.jmccs.tray;
 
+import de.pitkley.jmccs.monitor.Monitor;
+import de.pitkley.jmccs.monitor.MonitorManager;
+import de.pitkley.jmccs.monitor.Monitors;
+
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 
 public class Main {
+
+    final static MonitorManager monitorManager = Monitors.getMonitorManager();
+    final static List<Monitor> monitors = monitorManager.getMonitors();
+
+    private static class Brightness {
+        final int mainBrightness;
+        final int otherBrightness;
+
+        Brightness(int brightness) {
+            this(brightness, brightness);
+        }
+
+        Brightness(int mainBrightness, int otherBrightness) {
+            this.mainBrightness = mainBrightness;
+            this.otherBrightness = otherBrightness;
+        }
+    }
+
+    private static void setBrightness(Brightness brightness) {
+        for (Monitor monitor : monitors) {
+            if (monitor.isMainMonitor()) {
+                monitor.setBrightness(brightness.mainBrightness);
+            } else {
+                monitor.setBrightness(brightness.otherBrightness);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         if (!SystemTray.isSupported()) {
@@ -16,22 +49,25 @@ public class Main {
         final TrayIcon icon = new TrayIcon(i);
         final SystemTray tray = SystemTray.getSystemTray();
 
-        MenuItem aboutItem = new MenuItem("About");
-        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+        final Map<String, Brightness> menuItems = new TreeMap<>();
+        menuItems.put("100 - 100", new Brightness(100));
+        menuItems.put("100 - 1", new Brightness(100, 1));
+        menuItems.put("10 - 1", new Brightness(10, 1));
+        menuItems.put("1 - 1", new Brightness(1));
 
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
+        MenuItem miExit = new MenuItem("Exit");
+        miExit.addActionListener(e -> {
+            monitorManager.closeMonitors();
+            System.exit(0);
+        });
+        popup.add(miExit);
+        popup.addSeparator();
 
-        popup.add(aboutItem);
-        popup.addSeparator();
-        popup.add(cb1);
-        popup.add(cb2);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
+        for (Map.Entry<String, Brightness> entry : menuItems.entrySet()) {
+            MenuItem m = new MenuItem(entry.getKey());
+            m.addActionListener(e -> Main.setBrightness(entry.getValue()));
+            popup.add(m);
+        }
 
         icon.setPopupMenu(popup);
 
@@ -39,6 +75,7 @@ public class Main {
             tray.add(icon);
         } catch (AWTException e) {
             e.printStackTrace();
+            monitorManager.closeMonitors();
         }
     }
 }
